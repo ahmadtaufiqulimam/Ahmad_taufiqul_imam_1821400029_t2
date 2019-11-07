4bbc5371c35c812dd41086343ab0a7617af543fd
ahmadtaufiqulimam <57491836+ahmadtaufiqulimam@users.noreply.github.com>
2019-11-07 21:02:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/putardadu/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.putardadu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btAcak ;
+    private TextView txtView;
+    private ImageView gbrView ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btAcak = findViewById(R.id.btAcak);
+        txtView = findViewById(R.id.txtView);
+        gbrView = findViewById(R.id.gbrView);
+
+        btAcak . setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //toast ();
+                melemparDadu ();
+                gantiGambar ();
+               
+                
+
+            }
+        });
+    }
+
+    private void gantiGambar() {
+        Random Acak = new Random();
+       // Membuat Array Drawable (Gambar) Dadu
+      int [] gambar = new int[6];
+      gambar[0]=R.drawable.dice_1;
+      gambar[1]=R.drawable.dice_2;
+      gambar[2]=R.drawable.dice_3;
+      gambar[3]=R.drawable.dice_4;
+      gambar[4]=R.drawable.dice_5;
+      gambar[5]=R.drawable.dice_6;
+      //Mengeset Gambar dengan Drawable Baru + diacak
+      gbrView.setImageResource(gambar [Acak.nextInt(5)] );
+
+    }
+
+    private void melemparDadu() {
+        Random Acak = new Random();
+        String angka = String.valueOf(1+ Acak.nextInt(6));
+
+
+        if (txtView.getText().equals("6")) {
+            txtView.setText("6");
+            Toast.makeText(MainActivity.this, "Maximal", Toast.LENGTH_SHORT).show();
+            
+        }else {
+            txtView .setText(angka);
+            Toast.makeText(MainActivity.this, "Belum Maximal", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private void toast() {
+        Toast .makeText(MainActivity.this ,
+                "Hello", Toast.LENGTH_SHORT ).show();
+    }
+}
